bf04888707bf20e538627b3d9ede11a45123e9bc
Gavin Brewer <gavinbr.orchid@gmail.com>
2020-07-26 17:45:12 +0100

    Added tests and functionality for reporting on frequency of word usage.

--- src/main/java/WordAnalyzer.java
+++ src/main/java/WordAnalyzer.java
@@ -36,4 +36,57 @@ public class WordAnalyzer {
 
         return statsDictionary;
     }
+
+    public double getAverage(String str) {
+        String[] stringList = str.split("\\s");
+        List<Integer> wordLengths = new ArrayList<Integer>();
+
+        for (String word : stringList)
+        {
+            wordLengths.add(word.length());
+        }
+
+        int sum = 0;
+        for(int wordLength : wordLengths)
+            sum += wordLength;
+
+        return (double) sum / wordLengths.size();
+    }
+
+    public int getMostFrequentWordLength(String str) {
+        Hashtable<Integer, Integer> statistics = getStatistics(str);
+
+        //Find the most frequent word length
+        int max = 0;
+        for (Integer key : statistics.keySet())
+        {
+            Integer tmp = statistics.get(key);
+            if (tmp.compareTo(max) > 0)
+            {
+                max = tmp;
+            }
+        }
+
+        return max;
+    }
+
+    public List<Integer> getMaximalWordLengths(String str) {
+        Hashtable<Integer, Integer> statistics = getStatistics(str);
+        List<Integer> maximalElements = new ArrayList<Integer>();
+
+        Integer max = getMostFrequentWordLength(str);
+
+        //Obtain the lengths of words that have this frequency
+        for (HashMap.Entry<Integer, Integer> entry : statistics.entrySet())
+        {
+            if (entry.getValue().equals(max))
+            {
+                maximalElements.add(entry.getKey());
+            }
+        }
+
+        Collections.sort(maximalElements);
+
+        return maximalElements;
+    }
 }
--- src/test/java/WordAnalyzerTest.java
+++ src/test/java/WordAnalyzerTest.java
@@ -23,4 +23,29 @@ public class WordAnalyzerTest {
         Assertions.assertEquals(1, wordDictionary.get(3));
     }
 
+    @Test
+    public void testGetAverage() {
+        WordAnalyzer wordAnalyzer = new WordAnalyzer();
+        double average = wordAnalyzer.getAverage("This is a test run");
+
+        Assertions.assertEquals(2.8, average);
+    }
+
+    @Test
+    public void testGetMostFrequentWordLength() {
+        WordAnalyzer wordAnalyzer = new WordAnalyzer();
+        int mostFrequentWordLength = wordAnalyzer.getMostFrequentWordLength("Hello world & good morning. The date is 18/05/2016");
+
+        Assertions.assertEquals(2, mostFrequentWordLength);
+    }
+
+    @Test
+    public void testGetMaximalWordLengths() {
+        WordAnalyzer wordAnalyzer = new WordAnalyzer();
+        List<Integer> maximalWordLengths = wordAnalyzer.getMaximalWordLengths("Hello world & good morning. The date is 18/05/2016");
+
+        Assertions.assertEquals(4, maximalWordLengths.get(0));
+        Assertions.assertEquals(5, maximalWordLengths.get(1));
+    }
+
 }
